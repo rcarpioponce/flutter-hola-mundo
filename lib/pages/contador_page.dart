35544eb970d43b9910a02a5b0fd9cef56c8eaf31
Renzo R. Carpio Ponce <me@renzocarpio.com>
2019-05-26 11:08:15 -0500

    funcionalidad 3 botones state _contador

--- lib/pages/contador_page.dart
+++ lib/pages/contador_page.dart
@@ -35,16 +35,31 @@ class _ContadorPageState extends State<ContadorPage>{
         )
       ),
       //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: (){
-          _conteo++;
-
-          setState(() {
-            
-          });
-        },
-      ),
+      floatingActionButton: _crearBotones()
     );
   }
+  Widget _crearBotones(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30.0),
+        FloatingActionButton( child: Icon(Icons.exposure_zero), onPressed: _reset),
+        Expanded(child: SizedBox()),
+        FloatingActionButton( child: Icon(Icons.remove), onPressed: _sustraer),
+        SizedBox( width: 5.0),
+        FloatingActionButton( child: Icon(Icons.add), onPressed: _agregar),
+      ],
+    );
+  }
+
+  void _agregar(){
+    setState(() => _conteo++);
+  }
+  void _sustraer(){
+    setState(() => _conteo--);
+  }
+  void _reset(){
+    setState(() => _conteo = 0 );
+  }    
 }
+
